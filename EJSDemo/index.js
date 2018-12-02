a46bf05eb2adaa26ed53121da22457a472c2ab67
Urehelonn <cavymouse@163.com>
2018-12-02 07:16:28 +0000

    style sheet and other pages

--- EJSDemo/index.js
+++ EJSDemo/index.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var app=express();
-
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 })
 
 app.get("/fuzzyfriend/:fuzz", function(req, res){
     var friends=req.params.fuzz;
-    res.render("friends.ejs",{friends});
-})
+    res.render("friends",{friends});
+});
 
+app.get("/posts/", function(req, res) {
+   var post = [
+       {title: "ASD", author: "Author"},
+       {title: "QWE", author: "Cavy"},
+       {title: "UIO", author: "Adam"},
+       {title: "HJK", author: "Bob"}
+       ];
+   res.render("post", {posts:post});
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("listening");
