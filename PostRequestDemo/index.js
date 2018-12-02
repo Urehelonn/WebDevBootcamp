2941f0e0ddcc7c0eac244e59bc6525efcd984cdb
Urehelonn <cavymouse@163.com>
2018-12-02 10:20:49 +0000

    EJS demo prac finish

---
+++ PostRequestDemo/index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+
+var fuzz=[
+   "qwe",
+   "ert",
+   "poi",
+   "oij"
+   ];
+       
+       
+app.get("/", function(req, res){
+   res.render("home", {fuzz}); 
+});
+
+app.post("/addFuzz", function(req, res){
+    fuzz.push(req.body.name);
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server starts...");
+});
