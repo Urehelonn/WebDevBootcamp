6bd6b4b3a4a74e775e671d289eef39f38f2341e9
Urehelonn <cavymouse@163.com>
2018-12-03 23:39:38 +0000

    Monster Hunter Page building starts

---
+++ YelpCamp/YelpCamp_v1/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+app.get("/vaal_hazak_armor", function(req, res){
+    var armor=[
+           {name: "Vaal Hazak Alpha Armor Set", img: "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/vaal_hazak_alpha_armor_set_mhw_small.png", setBonus:"Vaal Hazak Vitlity"},
+           {name: "Vaal Hazak Beta Armor Set", img: "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/vaal_hazak_beta_armor_set_mhw_small.png", setBonus:"Vaal Hazak Vitlity"},
+           {name: "Vaal Hazak Gamma Armor Set", img: "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/vaal_hazak_alpha_armor_set_mhw_small.png", setBonus:"Vaal Hazak Vitlity"}
+        ];
+        res.render("armor", {armor});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('sever starts');
+});
